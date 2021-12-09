b04b83cf5921b3fa617adcb59781119a98feb9b2
mmalla <manisha.malla@gmail.com>
2021-12-09 16:40:41 +0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,12 +6,14 @@ import React from "react";
 import Layout from "components/common/Layout";
 import ContextProvider from "lib/context";
 import "styles.css";
+import Zipy from "zipyai";
 
 if (typeof window !== "undefined") {
   require("lazysizes/plugins/attrchange/ls.attrchange.js");
   require("lazysizes/plugins/respimg/ls.respimg.js");
   require("lazysizes");
 }
+Zipy.init("c784b808");
 
 const MyApp = ({ Component, pageProps }) => (
   <>
