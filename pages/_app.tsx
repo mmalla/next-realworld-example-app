1aada6a8e2a9351eb374f048ed1c106638ab89f8
mmalla <manisha.malla@gmail.com>
2022-05-11 08:48:22 +0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,7 @@ if (typeof window !== "undefined") {
   require("lazysizes/plugins/attrchange/ls.attrchange.js");
   require("lazysizes/plugins/respimg/ls.respimg.js");
   require("lazysizes");
-  Zipy.init("ac4d3a83", {releaseVer: "Conduit v13.0"});
+  Zipy.init("ac4d3a83", {releaseVer: "Conduit v13.1"});
 }
 
 const MyApp = ({ Component, pageProps }) => (
