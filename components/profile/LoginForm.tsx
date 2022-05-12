5c6571eb6fabea4f05174850d24cc991587753da
mmalla <manisha.malla@gmail.com>
2022-05-12 10:35:35 +0400

    Update LoginForm.tsx

--- components/profile/LoginForm.tsx
+++ components/profile/LoginForm.tsx
@@ -5,7 +5,7 @@ import { mutate } from "swr";
 import ListErrors from "../common/ListErrors";
 import UserAPI from "../../lib/api/user";
 
-import Zipy from "zipy-staging-nextjs";
+import Zipy from "zipy-test-v1";
 
 const LoginForm = () => {
   const [isLoading, setLoading] = React.useState(false);
